965510941556e53c27efabc0debb8691352e2d3d
Shriyansh-Ag <Shriyansh.mait@gmail.com>
2026-07-01 19:55:09 +0530

    Initial Commit

---
+++ Atlas/AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasApp.swift
+//  Atlas
+//
+//  Created by Shriyansh Agarwal on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
